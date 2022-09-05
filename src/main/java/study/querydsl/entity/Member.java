ee970cd788324a2e8e00d5909808b8530768146f
jiwonAn1 <jwan0131@gmail.com>
2022-09-05 13:04:22 +0900

    Entity 생성

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,50 @@
+package study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member(Long id, String username, int age) {
+        this.id = id;
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,32 @@
+package study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public Team(Long id, String name, List<Member> members) {
+        this.id = id;
+        this.name = name;
+        this.members = members;
+    }
+}
